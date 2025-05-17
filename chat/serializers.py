07ad5cd7ba568f692ef78b8cdec5ce2639c39551
sagar sangwan <iamsagarsangwan@gmail.com>
2025-05-17 08:41:20 +0530

    sending participants details with chatroom to frontend

--- chat/serializers.py
+++ chat/serializers.py
@@ -2,15 +2,73 @@ from rest_framework import serializers
 
 
 from .models import ChatMessage, ChatRoom
+from django.contrib.auth.models import User
+
+from allauth.socialaccount.models import SocialAccount
+
+
+class SocialAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialAccount
+        fields = ["extra_data"]
+        read_only = True  # These fields are typically managed by allauth
+
+
+class UserSerializer(serializers.ModelSerializer):
+    social_accounts = SocialAccountSerializer(
+        many=True, read_only=True, source="socialaccount_set"
+    )
+
+    class Meta:
+        model = User
+        # fields = ["id", "username", "email", "username", "picture"]
+        fields = ["id", "username", "social_accounts"]
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    other_participant = serializers.SerializerMethodField()
+
     class Meta:
         model = ChatRoom
-        fields = "__all__"
+        # fields = "__all__"
+        fields = [
+            "id",
+            "participants",
+            "name",
+            "is_group",
+            "created_at",
+            "updated_at",
+            "slug",
+            "other_participant",
+        ]
+
+    def get_other_participant(self, obj):
+        """
+        Returns the serialized details of the other participant in a one-on-one chat room.
+        """
+        if obj.is_group:
+            return None
+
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            current_user = request.user
+            other_participant = None
+            for participant in obj.participants.all():
+                if participant != current_user:
+                    other_participant = participant
+                    break
+            if other_participant:
+                serializer = UserSerializer(other_participant, context=self.context)
+
+                return serializer.data
+                # return other_participant.social_account.extra_data
+        return None
 
 
 class ChatMessagesSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+
     class Meta:
         model = ChatMessage
         fields = "__all__"
